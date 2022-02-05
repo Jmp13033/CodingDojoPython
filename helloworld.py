62e7617b7c3475c82e7177c4ee1dd1488562a935
jared peck <jared.peck@uconn.edu>
2022-02-05 10:16:41 -0500

    python basics

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+print("hello world ") #1a
+name = "jared"
+print("hello" + name) #2a
+
+num= 4
+print("hello" + str(num)) #4a
+
+
+food1 = "talapia"
+food2 = "cod"
+
+
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
---
+++ loopbasics1.py
@@ -0,0 +1,32 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range(1001):
+#     if i % 5 == 0:
+#         print(i)
+
+# for i in range(101):
+#     if i % 5 == 0:
+#         print("coding")
+#     elif i % 10:
+#         print("coding dojo")
+
+# sum = 0 
+
+# for i in range(500000):
+#     if i % 2 == 1:
+#         sum += i 
+
+# print(sum)
+
+# for i in range(2018,0,-4):
+#     print(i)
+
+# low_number = 0
+# high_number = 1000
+# mult = 20
+# for i in range(low_number,high_number):
+#     if i % mult == 0:
+#         print(i)
+
+
