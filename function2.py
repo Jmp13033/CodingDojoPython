618a5f2a2076502c2b0e2fdb956966c6f336512c
jared peck <jared.peck@uconn.edu>
2022-02-05 16:55:29 -0500

    function2

---
+++ function2.py
@@ -0,0 +1,44 @@
+# def number(x):
+#     for i in range(x,0,-1):
+#         print(i)
+# print(number(5))
+
+# def num(x):
+#     print(x[0])
+#     return x[1]
+
+# print(num([9,8]))
+
+# def length(x):
+#     num = x[0]
+#     length = len(x)
+#     return num + length
+
+# print(length([9,8]))
+
+# def number_greater_than_second(x):  # [0,9,8]
+#     value = x[2:]
+#     if len(x) < 2:
+#         return -1
+#     else:
+#         return value
+
+# print(number_greater_than_second([8,8,9]))
+
+
+# def length_value(x,y): # return a list that has the value of this length  and that value
+#     num = []
+#     for i in range(x):
+#         num.append(y)
+#     return num
+
+# print(length_value(6,6))
+
+
+
+
+
+
+
+
+
