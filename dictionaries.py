e271d797e13ca6508749b357839175acdc542faa
jared peck <jared.peck@uconn.edu>
2022-02-05 22:27:39 -0500

    functionswork

--- dictionaries.py
+++ dictionaries.py
@@ -11,36 +11,36 @@
 
 # # x[1][0] = 15
 
-# # # students[0]["last_name"] = "Bryan" 
-# # # print(students)
+# # students[0]["last_name"] = "Bryan" 
+# # print(students)
 
-# # # sports_directory['soccer'][0] = "Andres"
-# # # print(sports_directory)
+# # sports_directory['soccer'][0] = "Andres"
+# # print(sports_directory)
 
-# # # z[0]["y"] = 30
+# # z[0]["y"] = 30
 
-# # # print(z)
+# # print(z)
 
-# students = [
-# {'first_name':  'Michael', 'last_name' : 'Jordan'},
-# {'first_name' : 'John', 'last_name' : 'Rosales'},
-# {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-# {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
+# # students = [
+# # {'first_name':  'Michael', 'last_name' : 'Jordan'},
+# # {'first_name' : 'John', 'last_name' : 'Rosales'},
+# # {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+# # {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# #     ]
 
-# for i in range(0,len(students)):
-#     print(students[i]["first_name"])
-#     print(students[i]["last_name"])
+# # for i in range(0,len(students)):
+# #     print(students[i]["first_name"])
+# #     print(students[i]["last_name"])
 
     
 
 
 
-# index = 0 # set the index variable
-# while index < len(students): # this will get the index
-#     for key in students[index]: # get the key in the variable while the index increases ++
-#         print(students[index][key]) # print the index and key.. 
-#     index += 1 # increase thevalue of index 
+# # index = 0 # set the index variable
+# # while index < len(students): # this will get the index
+# #     for key in students[index]: # get the key in the variable while the index increases ++
+# #         print(students[index][key]) # print the index and key.. 
+# #     index += 1 # increase thevalue of index 
 
 
 
@@ -51,14 +51,12 @@
 # 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 # }
 
-# for key, values in dojo.items():  # iterate through the dojo items
-#     print("key" + " " +  key) # print the key 
-#     if(isinstance(values,list)): # if it is an instance of a list, print the items / instance of a lidt
-#         for value in values: # for all the values in the list printthe valules
-#             print(value) # print values
 
+# for key, values in dojo.items():
+#     if(isinstance(values,list)): 
+#         for value in values:
+#             print(value)
+# print("instructors")
+# print(len(dojo["instructors"]))
+# print(len(dojo["locations"]))
 
-
-
-
-    
