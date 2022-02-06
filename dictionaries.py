5bd7c5e483c298ad471f3ce03196c4a73bfe1577
jared peck <jared.peck@uconn.edu>
2022-02-06 16:23:31 -0500

    dictionarys

--- dictionaries.py
+++ dictionaries.py
@@ -21,12 +21,19 @@
 
 # # print(z)
 
-# # students = [
-# # {'first_name':  'Michael', 'last_name' : 'Jordan'},
-# # {'first_name' : 'John', 'last_name' : 'Rosales'},
-# # {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-# # {'first_name' : 'KB', 'last_name' : 'Tonel'}
-# #     ]
+# students = [
+# {'first_name':  'Michael', 'last_name' : 'Jordan'},
+# {'first_name' : 'John', 'last_name' : 'Rosales'},
+# {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+# {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+# def iterated(students):
+#     for student in students:
+#             key1 = "first_name"
+#             key2 = "last_name"
+#             print(key1, ",", student[key1], "-", key2, student[key2])
+
+# iterated(students)
 
 # # for i in range(0,len(students)):
 # #     print(students[i]["first_name"])
@@ -46,17 +53,17 @@
 
 
 
-# dojo = {
-# 'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-# 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-# }
+# # dojo = {
+# # 'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+# # 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# # }
 
 
-# for key, values in dojo.items():
-#     if(isinstance(values,list)): 
-#         for value in values:
-#             print(value)
-# print("instructors")
-# print(len(dojo["instructors"]))
-# print(len(dojo["locations"]))
+# # for key, values in dojo.items():
+# #     if(isinstance(values,list)): 
+# #         for value in values:
+# #             print(value)
+# # print("instructors")
+# # print(len(dojo["instructors"]))
+# # print(len(dojo["locations"]))
 
