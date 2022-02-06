cee608f73a256a4cda84b57808434b3d6653ba05
jared peck <jared.peck@uconn.edu>
2022-02-05 21:43:17 -0500

    functions intermediate

--- dictionaries.py
+++ dictionaries.py
@@ -1,53 +1,61 @@
-x = [ [5,2,3], [10,8,9] ] 
-students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
-]
-sports_directory = {
-    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-}
-z = [ {'x': 10, 'y': 20} ]
+# # x = [ [5,2,3], [10,8,9] ] 
+# # students = [
+# #      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+# #      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# # ]
+# # sports_directory = {
+# #     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+# #     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# # }
+# # z = [ {'x': 10, 'y': 20} ]
 
-x[1][0] = 15
+# # x[1][0] = 15
 
-students[0]["last_name"] = "bryan" 
-print(students)
+# # # students[0]["last_name"] = "Bryan" 
+# # # print(students)
 
-sports_directory['soccer'][0] = "Andres"
-print(sports_directory)
+# # # sports_directory['soccer'][0] = "Andres"
+# # # print(sports_directory)
 
-z[0]["y"] = 30
+# # # z[0]["y"] = 30
 
-print(z)
+# # # print(z)
 
-students = [
-{'first_name':  'Michael', 'last_name' : 'Jordan'},
-{'first_name' : 'John', 'last_name' : 'Rosales'},
-{'first_name' : 'Mark', 'last_name' : 'Guillen'},
-{'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
+# students = [
+# {'first_name':  'Michael', 'last_name' : 'Jordan'},
+# {'first_name' : 'John', 'last_name' : 'Rosales'},
+# {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+# {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
 
-index = 0 # set the index variable
-while index < len(students): # this will get the index
-    for key in students[index]: # get the key in the variable while the index increases ++
-        print(students[index][key]) # print the index and key.. 
-    index += 1 # increase thevalue of index 
+# for i in range(0,len(students)):
+#     print(students[i]["first_name"])
+#     print(students[i]["last_name"])
 
-
+    
 
 
 
-dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
+# index = 0 # set the index variable
+# while index < len(students): # this will get the index
+#     for key in students[index]: # get the key in the variable while the index increases ++
+#         print(students[index][key]) # print the index and key.. 
+#     index += 1 # increase thevalue of index 
 
-for key, values in dojo.items():  # iterate through the dojo items
-    print("key" + key) # print the key 
-    if(isinstance(values,list)): # if it is an instanse of a list, print the items / instance of a lidt
-        for value in values: # for all the values in the list printthe valules
-            print(value) # print values
+
+
+
+
+# dojo = {
+# 'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+# 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+
+# for key, values in dojo.items():  # iterate through the dojo items
+#     print("key" + " " +  key) # print the key 
+#     if(isinstance(values,list)): # if it is an instance of a list, print the items / instance of a lidt
+#         for value in values: # for all the values in the list printthe valules
+#             print(value) # print values
 
 
 
